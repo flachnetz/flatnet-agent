457e8c84402a9d9ba22bcf32d2b3048d69e6ad07
Oliver Bestmann <oliver.bestmann@googlemail.com>
2016-04-25 21:42:44 +0200

    Small clenups, remove global variables, split into multiple files, add NameProvider, add graceful shutdown.

---
+++ capture.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"time"
+)
+
+type tcpCapture struct {
+	device   string
+	closed   chan bool
+	packages chan<- *NetPackage
+	handle   *pcap.Handle
+}
+
+func StartTcpCapture(device string, target chan<- *NetPackage) (*tcpCapture, error) {
+	handle, err := pcap.OpenLive(device, 128, true, 500)
+	if err != nil {
+		return nil, err
+	}
+
+	capture := &tcpCapture{
+		device:   device,
+		handle:   handle,
+		closed:   make(chan bool, 0),
+		packages: target,
+	}
+
+	go capture.run()
+	return capture, nil
+}
+
+func (tc *tcpCapture) Close() error {
+	tc.handle.Close()
+
+	log.Info("Waiting for capture loop to terminate")
+	<-tc.closed
+	return nil
+}
+
+func (tc *tcpCapture) run() {
+	defer close(tc.closed)
+
+	// create a parser for a simple network stack
+	var ipv4 layers.IPv4
+	var eth layers.Ethernet
+	var tcp layers.TCP
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ipv4, &tcp)
+
+	// make network packages available as a channel
+	packetSource := gopacket.NewPacketSource(tc.handle, tc.handle.LinkType())
+
+	var decoded []gopacket.LayerType
+	for pkt := range packetSource.Packets() {
+		parser.DecodeLayers(pkt.Data(), &decoded)
+
+		netPackage := &NetPackage{Packages: 1}
+		for _, layerType := range decoded {
+			switch layerType {
+			case layers.LayerTypeTCP:
+				packageLength := len(tcp.Payload)
+				if packageLength > 0 {
+					netPackage.Timestamp = time.Now().UnixNano() / int64(time.Millisecond)
+					netPackage.Len = uint16(packageLength)
+					netPackage.Source.IP = ipv4.SrcIP.String()
+					netPackage.Source.Port = uint16(tcp.SrcPort)
+					netPackage.Destination.IP = ipv4.DstIP.String()
+					netPackage.Destination.Port = uint16(tcp.DstPort)
+				}
+			}
+		}
+
+		if netPackage.Len > 0 {
+			// send non-blocking, drop packages if consumer queue is full.
+			select {
+			case tc.packages <- netPackage:
+			default:
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,41 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
+	"flag"
 	"os"
 	"os/signal"
-	"sync"
-	"time"
-	"flag"
-	"github.com/Shopify/sarama"
 	"strings"
-	"encoding/json"
-	"net"
-	"strconv"
+	"time"
+
+	"io"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/VividCortex/godaemon"
+	"github.com/google/gopacket/pcap"
 )
-
-var (
-	wg sync.WaitGroup
-	ignoredAddresses map[string][]uint16
-	aggregatedPackages map[netAggregationKey]*NetPackage = make(map[netAggregationKey]*NetPackage)
-	aggregatedPackagesChan chan *NetPackage = make(chan *NetPackage, 2048)
-)
-
-type netAggregationKey struct {
-	source      Service
-	destination Service
-}
 
 type NetPackages struct {
 	Timestamp        int64
-	ServicePackages  []NetPackage
+	ServicePackages  []*NetPackage
 	DurationInMillis int
-	encoded          []byte
+
+	encoded []byte
 }
 type Service struct {
 	Name string
@@ -51,194 +35,180 @@ type NetPackage struct {
 	Timestamp   int64
 }
 
-func (n *NetPackages) Length() int {
-	if n != nil && n.encoded == nil {
-		// todo error handling
-		n.encoded, _ = n.Encode()
-	}
-	return len(n.encoded)
-}
-
-func (n *NetPackages) Encode() ([]byte, error) {
-	return json.Marshal(n)
-}
-
-func shutDownHook() {
-	go func() {
-		signals := make(chan os.Signal, 1)
-		signal.Notify(signals, os.Interrupt, os.Kill)
-		<-signals
-		fmt.Println("Done.")
-		wg.Done()
-	}()
-}
-
-func producerTicker(brokers, prefixes []string) {
-	durationInMillis := 2000
-	ticker := time.Tick(time.Duration(durationInMillis) * time.Millisecond)
-
-	config := sarama.NewConfig()
-	config.Producer.RequiredAcks = sarama.WaitForLocal       // Only wait for the leader to ack
-	config.Producer.Compression = sarama.CompressionSnappy   // Compress messages
-	config.Producer.Flush.Frequency = 500 * time.Millisecond // Flush batches every 500ms
-
-	// todo error handling
-	producer, err := sarama.NewAsyncProducer(brokers, config)
-	if err != nil {
-		log.WithError(err).Fatal("Could not connect to kafka broker")
-		return
+func aggregate(packages <-chan *NetPackage, consumer chan<- *NetPackages, nameProvider NameProvider) {
+	type aggregationKey struct {
+		source      Service
+		destination Service
 	}
 
-	defer producer.Close()
+	// aggregate for a few seconds before sending a packet
+	aggregationInterval := 2 * time.Second
+	ticker := time.NewTicker(aggregationInterval)
+	defer ticker.Stop()
 
+	aggregated := make(map[aggregationKey]*NetPackage)
 	for {
 		select {
-		case now := <-ticker:
-			aggregatedPackagesList := []NetPackage{}
-			for _, p := range aggregatedPackages {
-				p.Timestamp = now.Unix()
-				aggregatedPackagesList = append(aggregatedPackagesList, *p)
-			}
-			aggregatedPackages = make(map[netAggregationKey]*NetPackage)
+		case now := <-ticker.C:
+			if len(aggregated) > 0 {
+				aggregatedPackagesList := []*NetPackage{}
+				for _, p := range aggregated {
+					aggregatedPackagesList = append(aggregatedPackagesList, p)
+				}
 
-			netPackages := &NetPackages{
-				Timestamp: now.Unix(),
-				ServicePackages: aggregatedPackagesList,
-				DurationInMillis: durationInMillis}
-			if len(netPackages.ServicePackages) > 0 {
-				log.Info(netPackages)
-				producer.Input() <- &sarama.ProducerMessage{
-					Topic: "flatnet_log",
-					Key:   nil,
-					Value: netPackages,
+				// reset the aggregation map
+				aggregated = make(map[aggregationKey]*NetPackage)
+
+				result := &NetPackages{
+					Timestamp:        now.UnixNano() / int64(time.Millisecond),
+					ServicePackages:  aggregatedPackagesList,
+					DurationInMillis: int(aggregationInterval / time.Millisecond),
+				}
+
+				// send non-blocking, drop packages if consumer queue is full.
+				select {
+				case consumer <- result:
+				default:
 				}
 			}
 
-		case netPackage := <-aggregatedPackagesChan:
-			if p, exists := aggregatedPackages[netAggregationKey{netPackage.Source, netPackage.Destination}]; exists == false {
-				aggregatedPackages[netAggregationKey{netPackage.Source, netPackage.Destination}] = netPackage
-			} else {
-				p.Len = p.Len + netPackage.Len
+		case netPackage, ok := <-packages:
+			if !ok {
+				// end of stream reached...
+				return
+			}
+
+			key := aggregationKey{netPackage.Source, netPackage.Destination}
+
+			if p, exists := aggregated[key]; exists {
+				p.Len += netPackage.Len
 				p.Packages += 1
+			} else {
+				aggregated[key] = netPackage
+
+				netPackage.Source.Name = nameProvider.GetName(netPackage.Source.IP, netPackage.Source.Port)
+				netPackage.Destination.Name = nameProvider.GetName(netPackage.Destination.IP, netPackage.Destination.Port)
 			}
 		}
 	}
-
 }
 
 func main() {
 	flagConsul := flag.String("consul", "", "Address of consul")
 	flagDaemon := flag.Bool("daemon", false, "Start flatnet in the background")
+	flagVerbose := flag.Bool("verbose", false, "Activate verbose logging")
+	flagLogfile := flag.String("logfile", "", "Set this to redirect logging into a logfile")
 	flagBrokers := flag.String("brokers", "docker:9092,192.168.59.100:9092", "The Kafka brokers to connect to, as a comma separated list")
-	flagPrefixes := flag.String("prefix", "", "Only listen on interfaces having one of the given prefixes. " +
+	flagPrefixes := flag.String("interfaces", "", "Only listen on interfaces having one of the given prefixes. "+
 		"Multiple prefixes can be specified as a comma separated list.")
 
 	flag.Parse()
 
 	if *flagDaemon {
+		log.Info("Daemonizing process into background now")
 		godaemon.MakeDaemon(&godaemon.DaemonAttr{})
+	}
+
+	if *flagVerbose {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	gracefulShutdown := shutdownSignalHandler()
+
+	if *flagLogfile != "" {
+		file, err := os.OpenFile(*flagLogfile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+		if err != nil {
+			log.WithError(err).Warn("Could not open logfile")
+		} else {
+			defer file.Close()
+			log.SetOutput(file)
+		}
+	}
+
+	var nameProvider NameProvider = &NoopNameProvider{}
+	if *flagConsul != "" {
+		var err error
+		nameProvider, err = NewConsulNameProvider(*flagConsul)
+		if err != nil {
+			log.Fatal("Could not initialize consul name provider")
+			return
+		}
 	}
 
 	brokers := strings.Split(*flagBrokers, ",")
 	prefixes := strings.Split(*flagPrefixes, ",")
 
-	devices, _ := pcap.FindAllDevs()
-	wg.Add(1)
-	shutDownHook()
-
-	ignoredAddresses = make(map[string][]uint16)
-	ignoredAddresses["31.24.96.135"] = []uint16{0}
-	ignoredAddresses["10.59.25.58"] = []uint16{9092}
-	for _, host := range brokers {
-		hostPort := strings.Split(host, ":")
-
-		addresses, _ := net.LookupHost(hostPort[0])
-		for _, address := range addresses {
-			port, _ := strconv.Atoi(hostPort[1])
-			ignoredAddresses[address] = []uint16{uint16(port)}
-		}
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.WithError(err).Fatal("Could not list interfaces")
+		return
 	}
-	log.WithField("addresses", ignoredAddresses).Info("Ignored addresses")
 
-	go producerTicker(brokers, prefixes)
+	packages := make(chan *NetPackage, 2048)
 
+	// start the package consumer. It willl take packages from the channel,
+	// aggregate them and send them to kafka. The resulting channel will be
+	// closed when the consumer finsihes his work.
+	producerClosed, err := startPackageConsumer(brokers, nameProvider, packages)
+	if err != nil {
+		log.WithError(err).Fatal("Could not connect to kafka broker")
+		return
+	}
+
+	var captures []io.Closer
 	for _, device := range devices {
-		matches := false
-		for _, prefix := range prefixes {
-			matches = matches || strings.HasPrefix(device.Name, prefix)
-		}
+		if shouldCapture(device, prefixes) {
+			logger := log.WithField("interface", device.Name)
 
-		if matches {
-			log.WithField("interface", device.Name).Info("Trying interface")
+			logger.Info("Trying interface")
+			capture, err := StartTcpCapture(device.Name, packages)
+			if err != nil {
+				logger.WithError(err).Warn("Could not open interface")
+				continue
+			}
 
-			go func(deviceName string) {
-				decoded := []gopacket.LayerType{}
-
-				var ipv4 layers.IPv4
-				var ipv6 layers.IPv6
-				var eth layers.Ethernet
-				var tcp layers.TCP
-				parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ipv4, &ipv6, &tcp)
-
-				h, err := pcap.OpenLive(deviceName, 65535, true, 500)
-				if err != nil {
-					log.Error("Could not open ", device, ": ", err)
-					return
-				}
-
-				defer h.Close()
-				log.WithField("interface", deviceName).Info("Listening to interface")
-				packetSource := gopacket.NewPacketSource(h, h.LinkType())
-				for pkt := range packetSource.Packets() {
-					parser.DecodeLayers(pkt.Data(), &decoded)
-					netPackage := &NetPackage{Source: Service{}, Destination: Service{}, Packages: 1}
-					for _, layerType := range decoded {
-						switch layerType {
-						case layers.LayerTypeTCP:
-							if len(tcp.Payload) > 0 {
-								netPackage.Source.IP = ipv4.SrcIP.String()
-								netPackage.Destination.IP = ipv4.DstIP.String()
-								netPackage.Len = ipv4.Length
-								netPackage.Timestamp = time.Now().Unix()
-								netPackage.Source.Port = uint16(tcp.SrcPort)
-								netPackage.Destination.Port = uint16(tcp.DstPort)
-
-								if *flagConsul != "" {
-									serviceName := ServiceName(*flagConsul,
-										netPackage.Source.IP, int(netPackage.Source.Port))
-									if serviceName != "" {
-										netPackage.Source.Name = serviceName
-									}
-
-									serviceName = ServiceName(*flagConsul,
-										netPackage.Destination.IP, int(netPackage.Destination.Port))
-									if serviceName != "" {
-										netPackage.Destination.Name = serviceName
-									}
-								}
-							}
-						}
-					}
-
-					if netPackage.Len > 0  && notIgnored(netPackage.Destination.IP, netPackage.Destination.Port) &&
-						notIgnored(netPackage.Source.IP, netPackage.Source.Port) {
-						aggregatedPackagesChan <- netPackage
-					}
-				}
-			}(device.Name)
+			logger.WithField("addresses", device.Addresses).Info("Capture started on interface")
+			captures = append(captures, capture)
 		}
 	}
-	wg.Wait()
+
+	// wait for the user to press ctrl-c
+	<-gracefulShutdown
+
+	for _, capture := range captures {
+		capture.Close()
+	}
+
+	// close down package stream and kafka
+	close(packages)
+	<-producerClosed
 }
 
-func notIgnored(ip string, port uint16) bool {
-	h := ignoredAddresses[ip]
-	if len(h) > 0 {
-		for _, p := range h {
-			if p == 0 || p == port {
-				return false
-			}
-		}
+func shouldCapture(device pcap.Interface, prefixes []string) bool {
+	matches := false
+	for _, prefix := range prefixes {
+		matches = matches || strings.HasPrefix(device.Name, prefix)
 	}
-	return true
+
+	return matches
+}
+
+func shutdownSignalHandler() <-chan os.Signal {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, os.Kill)
+
+	gracefulSignal := make(chan os.Signal, 1)
+
+	go func() {
+		signal := <-signals
+
+		log.Info("Sending graceful shutdown signal, press ctrl-c again to force quit")
+		gracefulSignal <- signal
+
+		// wait for force-quit signal
+		<-signals
+		log.Fatal("Force quitting...")
+	}()
+
+	return gracefulSignal
 }
